75b3aaf291b13c0b2c0b225845745e1328f5f9a9
Dmitry Draganov <draqanovdfeet@gmail.com>
2025-12-12 02:20:12 +0300

    added new pages and service template

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import './style.css';
+import { services } from './services';
+
+const servicesContainer = document.querySelector(
+  '[data-services-container]'
+) as HTMLDivElement;
+
+services.forEach((service) => {
+  const serviceElement = service.createElement();
+  servicesContainer.append(serviceElement);
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rolldownOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        portfolio: 'portfolio.html',
+        reviews: 'reviews.html',
+        services: 'services.html',
+        contacts: 'contacts.html',
+      },
+    },
+  },
 });
